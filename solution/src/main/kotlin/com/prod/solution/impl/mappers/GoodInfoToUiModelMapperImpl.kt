5814937f659fb319abaadc9d254787e56f4920be
PotaninPM <potaninpm@gmail.com>
2025-01-19 12:50:10 +0300

    5 task: GoodInfoToUiModelMapperImpl was done

--- solution/src/main/kotlin/com/prod/solution/impl/mappers/GoodInfoToUiModelMapperImpl.kt
+++ solution/src/main/kotlin/com/prod/solution/impl/mappers/GoodInfoToUiModelMapperImpl.kt
@@ -1,9 +1,14 @@
 package com.prod.solution.impl.mappers
 
+import android.graphics.Color
+import com.prod.core.api.Const
 import com.prod.core.api.domain.models.BonusInfo
 import com.prod.core.api.domain.models.GoodInfo
 import com.prod.core.api.domain.models.UserInfo
 import com.prod.core.api.mappers.GoodInfoToUiModelMapper
+import com.prod.core.api.ui.extensions.imageIdToResId
+import com.prod.core.api.ui.goods.GoodsBonusUi
+import com.prod.core.api.ui.goods.GoodsFavouriteUi
 import com.prod.core.api.ui.goods.GoodsItemUi
 
 /**
@@ -20,6 +25,73 @@ class GoodInfoToUiModelMapperImpl : GoodInfoToUiModelMapper {
         userInfo: UserInfo,
         bonusInfo: BonusInfo?,
     ): GoodsItemUi {
-        TODO("Implementation here")
+        val imageResId = imageIdToResId(goods.imageId)
+
+        val weight = "${goods.goodItemQuantityInfo.value} ${
+            when (goods.goodItemQuantityInfo.type) {
+                Const.TYPE_KILO -> "кг"
+                Const.TYPE_GRAMM -> "г"
+                else -> ""
+            }
+        }"
+
+        val price = "${goods.cost} ${Const.RUBBLE}"
+
+        val btnText = if (quantity > 0) {
+            val totCost = goods.cost * quantity
+            "${quantity}шт. = ${totCost} ${Const.RUBBLE}"
+        } else {
+            null
+        }
+
+        val label = when {
+            userInfo.favorites.contains(goods.id) -> GoodsFavouriteUi(
+                text = Const.FAVOURITE,
+                textColor = Color.BLACK
+            )
+            goods.isNew -> GoodsFavouriteUi(
+                text = Const.NEW,
+                textColor = Color.parseColor("#F8AA1B")
+            )
+            else -> null
+        }
+
+        val bonusInfo = bonusInfo?.let { bonus ->
+            val labelText = when (bonus.type) {
+                Const.TYPE_CASHBACK -> if (bonus.promotionExtra != null) {
+                    "${bonus.promotionExtra!!.label} • ${bonus.value}%"
+                } else {
+                    "Кэшбэк ${bonus.value}%"
+                }
+
+                Const.TYPE_POINTS -> if (bonus.promotionExtra != null) {
+                    "${bonus.promotionExtra!!.label} • ${bonus.value.toInt()} Б"
+                } else {
+                    "${bonus.value.toInt()} баллов"
+                }
+
+                else -> "${bonus.value.toInt()} баллов"
+            }
+
+            labelText.let {
+                GoodsBonusUi(
+                    text = it,
+                    textColor = bonus.promotionExtra?.tintColor?.let(Color::parseColor) ?: Color.BLACK,
+                    backgroundColor = bonus.promotionExtra?.baseColor?.let(Color::parseColor) ?: Color.WHITE
+                )
+            }
+        }
+
+        return GoodsItemUi(
+            id = goods.id,
+            name = goods.name,
+            imageRes = imageResId,
+            producerName = goods.producer.name,
+            quantityInfo = weight,
+            cost = price,
+            costAndQuantityInCart = btnText,
+            favourite = label,
+            bonus = bonusInfo
+        )
     }
 }
